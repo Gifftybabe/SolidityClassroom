e18e70d7656cd64d12fef4c35af6b332509a414e
Gifftybabe <ulokangozi@gmail.com>
2024-09-04 06:03:45 +0100

    mapping with struct

--- CourseWork/Mapping.sol
+++ CourseWork/Mapping.sol
@@ -13,4 +13,20 @@ contract Mapping {
 function getData(uint _rollNo) public view returns(string memory) {
     return data[_rollNo];
 }
+
+/ Mapping with struct
+
+struct Student {
+    string name;
+    uint rollNo;
+    bool pass;
+}
+mapping(uint => Student) studentDetails;
+
+function insertDetails(string memory _name, uint _rollNo, bool _pass) public {
+    studentDetails[_rollNo] = Student(_name, _rollNo, _pass);
+}
+function fetchDetails(uint _rollNo) public view returns(Student memory) {
+    return studentDetails[_rollNo];
+}
 }
